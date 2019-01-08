78a6c838efcc7a90d3b829b84717d908a5556b58
Takumi@Ubuntu <chibiwans@hotmail.com>
2019-01-08 21:19:49 +0900

    oldversion

--- example/test.cpp
+++ example/test.cpp
@@ -1,7 +1,7 @@
-#include "TPA.cpp"
+#include "../TPA.cpp"
 
 int main(){
-    TPA analyzer("/home/takumi/Desktop/PI_test/MAnalyze");
-    analyzer.reckon("/home/takumi/Desktop/PI_test");
+    TPA analyzer("/home/takumi/Desktop/data/test");
+    analyzer.reckon("/home/takumi/Desktop/data/MC");
     return 0;
 }
